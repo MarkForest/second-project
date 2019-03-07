21925b8a071ffd1498fece436c547265adaac6b6
Anatoliy Marchenko <toxerius7@gmail.com>
2019-03-07 19:03:15 +0200

    create layout project

---
+++ pages/functions.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Teacher
+ * Date: 07.03.2019
+ * Time: 18:35
+ */
+//Get Variables
+$page = 1;
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+}
